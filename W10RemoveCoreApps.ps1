bffc85c8a915697e937e00d979130c1414104991
Michael Chamberland <smokex365@gmail.com>
2015-12-19 23:15:54 -0600

    Create W10RemoveCoreApps.ps1

---
+++ W10RemoveCoreApps.ps1
@@ -0,0 +1,46 @@
+#3D Builder: 
+Get-AppxPackage *3dbuilder* | Remove-AppxPackage
+#Alarms and Clock: 
+#Get-AppxPackage *alarms* | Remove-AppxPackage
+#Calculator: 
+#Get-AppxPackage *calculator* | Remove-AppxPackage
+#Calendar and Mail: 
+Get-AppxPackage *communications* | Remove-AppxPackage
+#Camera: 
+Get-AppxPackage *camera* | Remove-AppxPackage
+#Get Office: 
+Get-AppxPackage *officehub* | Remove-AppxPackage
+#Get Skype: 
+Get-AppxPackage *skypeapp* | Remove-AppxPackage
+#Get Started: 
+Get-AppxPackage *getstarted* | Remove-AppxPackage
+#Groove Music: 
+Get-AppxPackage *zunemusic* | Remove-AppxPackage
+#Maps: 
+Get-AppxPackage *maps* | Remove-AppxPackage
+#Microsoft Solitaire Collection: 
+#Get-AppxPackage *solitairecollection* | Remove-AppxPackage
+#Money: 
+Get-AppxPackage *bingfinance* | Remove-AppxPackage
+#Movies & TV: 
+Get-AppxPackage *zunevideo* | Remove-AppxPackage
+#News: 
+Get-AppxPackage *bingnews* | Remove-AppxPackage
+#OneNote: 
+Get-AppxPackage *onenote* | Remove-AppxPackage
+#People: 
+Get-AppxPackage *people* | Remove-AppxPackage
+#Phone Companion: 
+Get-AppxPackage *windowsphone* | Remove-AppxPackage
+#Photos: 
+#Get-AppxPackage *photos* | Remove-AppxPackage
+#Store: 
+#Get-AppxPackage *windowsstore* | Remove-AppxPackage
+#Sports: 
+Get-AppxPackage *bingsports* | Remove-AppxPackage
+#Voice Recorder: 
+Get-AppxPackage *soundrecorder* | Remove-AppxPackage
+#Weather: 
+#Get-AppxPackage *bingweather* | Remove-AppxPackage
+#Xbox: 
+#Get-AppxPackage *xbox* | Remove-AppxPackage
