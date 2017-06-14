9f717206e3c7e0f1d4d41238493de916a27526fd
Michael Chamberland <smokex365@gmail.com>
2017-06-14 17:13:07 -0500

    added function to hide window

--- emptyrecyclebin-dates.ps1
+++ emptyrecyclebin-dates.ps1
@@ -1,3 +1,7 @@
+#Hides Window : http://jeffwouters.nl/index.php/2015/09/howto-hide-a-powershell-prompt/
+Add-Type -Name win -MemberDefinition '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);' -Namespace native
+[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle,0)
+
 ForEach ($Drive in Get-PSDrive -PSProvider FileSystem) {
     $Path = $Drive.Name + ':\$RECYCLE.BIN'
     Get-ChildItem $Path -Force -Recurse -ErrorAction SilentlyContinue |
