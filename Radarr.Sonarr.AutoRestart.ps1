9c86397a4aae5c19552064ad95bf6e821a822fdf
Michael Chamberland <smokex365@gmail.com>
2017-06-14 17:04:46 -0500

    Add files via upload

---
+++ Radarr.Sonarr.AutoRestart.ps1
@@ -0,0 +1,25 @@
+﻿#Checks for Administrator privileges and opens an elevated prompt is user has Administrator rights
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{   
+	$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+	Start-Process powershell -Verb runAs -ArgumentList $arguments
+	Break
+}
+# https://darshanaj.wordpress.com/2012/04/08/powershell-script-for-start-the-multiple-services-if-stopped/
+$srvservice=Get-Service
+$counter
+$counter=1
+foreach($service in $srvservice)
+	{
+		if ($srvservice[$counter].name -eq "Radarr" -or
+			$srvservice[$counter].name -eq "NzbDrone")
+			{
+			if ($srvservice[$counter].status -eq
+				"stopped"
+			)
+		{
+		start-service -name	$srvservice[$counter].name
+	}
+ }
+$counter++
+}
