529b7c9ef55dc64ed3f9fc8f4ca55a140db5b5f5
Michael Chamberland <smokex365@gmail.com>
2024-09-15 17:44:04 -0400

    updated

--- W10RemoveCoreApps.ps1
+++ W10RemoveCoreApps.ps1
@@ -1,51 +1,52 @@
-#3D Builder: 
-Get-AppxPackage *3dbuilder* | Remove-AppxPackage
-#Alarms and Clock: 
-#Get-AppxPackage *alarms* | Remove-AppxPackage
-#Calculator: 
-#Get-AppxPackage *calculator* | Remove-AppxPackage
-#Calendar and Mail: 
-Get-AppxPackage *communications* | Remove-AppxPackage
-#Camera: 
-Get-AppxPackage *camera* | Remove-AppxPackage
-#Get Office: 
-Get-AppxPackage *officehub* | Remove-AppxPackage
-#Get Skype: 
-Get-AppxPackage *skypeapp* | Remove-AppxPackage
-#Get Started: 
-Get-AppxPackage *getstarted* | Remove-AppxPackage
-#Groove Music: 
-Get-AppxPackage *zunemusic* | Remove-AppxPackage
-#Maps: 
-Get-AppxPackage *maps* | Remove-AppxPackage
-#Microsoft Solitaire Collection: 
-#Get-AppxPackage *solitairecollection* | Remove-AppxPackage
-#Money: 
-Get-AppxPackage *bingfinance* | Remove-AppxPackage
-#Movies & TV: 
-Get-AppxPackage *zunevideo* | Remove-AppxPackage
-#News: 
-Get-AppxPackage *bingnews* | Remove-AppxPackage
-#OneNote: 
-Get-AppxPackage *onenote* | Remove-AppxPackage
-#People: 
-Get-AppxPackage *people* | Remove-AppxPackage
-#Phone Companion: 
-Get-AppxPackage *windowsphone* | Remove-AppxPackage
-#Photos: 
-#Get-AppxPackage *photos* | Remove-AppxPackage
-#Sports: 
-Get-AppxPackage *bingsports* | Remove-AppxPackage
-#Voice Recorder: 
-Get-AppxPackage *soundrecorder* | Remove-AppxPackage
-#Weather: 
-#Get-AppxPackage *bingweather* | Remove-AppxPackage
-#Xbox: 
-#Get-AppxPackage *xbox* | Remove-AppxPackage
-#Sway
-Get-AppxPackage *sway* | Remove-AppxPackage
+$packages = (
+    #3D Builder:
+    "3dbuilder",
+    #Alarms and Clock: 
+    #"alarms",
+    #Calculator: 
+    #"calculator",
+    #Calendar and Mail: 
+    "communications",
+    #Camera: 
+    "camera",
+    #Get Office: 
+    "officehub",
+    #Get Skype: 
+    "skypeapp",
+    #Get Started: 
+    "getstarted",
+    #Groove Music: 
+    "zunemusic",
+    #Maps: 
+    "maps",
+    #Microsoft Solitaire Collection: 
+    #"solitairecollection",
+    #Money: 
+    "bingfinance",
+    #Movies & TV: 
+    "zunevideo",
+    #News: 
+    "bingnews",
+    #OneNote: 
+    "onenote",
+    #People: 
+    "people",
+    #Phone Companion: 
+    "windowsphone",
+    #Photos: 
+    #"photos",
+    #Sports: 
+    "bingsports",
+    #Voice Recorder: 
+    "soundrecorder",
+    #Weather: 
+    #"bingweather",
+    #Xbox: 
+    #"xbox",
+    #Sway
+    "sway"
+    )
 
-#Store: 
-#WARNING: Removing the Store app will prevent you from reinstalling any modern apps.
-#Leave it installed unless you know what you're doing!
-#Get-AppxPackage *windowsstore* | Remove-AppxPackage
+foreach ($apps in $packages) {   
+    Get-AppxPackage $appx | Remove-AppxPackage
+}
